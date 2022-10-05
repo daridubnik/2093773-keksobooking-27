250f9bc5c5281dce63f3475b551816b586a53117
Daria Dubnikova <daridubnik@gmail.com>
2022-10-05 17:13:17 +0300

    ДЗ 2. Вносит правки от наставника

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-function getRundomIntegralNumber(min, max) {
-  if (min < 0 || max < 0) {
+const getRandomNumber = (min, max, decimalPlace = 0) => {
+  if (min < 0 || max < 0 || min === max) {
     return NaN;
   }
-  if (max < min) {
-    return NaN;
-  }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
-getRundomIntegralNumber (1, 15);
-
-function getRundomNumber(min, max, decimalPlace) {
-  if (min < 0 || max < 0) {
-    return NaN;
+  if (min > max) {
+    [min, max] = [max, min];
   }
-  if (max < min) {
-    return NaN;
-  }
-  return Number((Math.random() * (max - min) + min).toFixed(decimalPlace));
-}
 
-getRundomNumber (1, 10, 2);
+  return +(Math.random() * (max - min + 1) + min).toFixed(decimalPlace);
+};
+
+getRandomNumber(1, 10, 2);
