203eb5827caff97b580c6526c108f646102b80ce
Daria Dubnikova <daridubnik@gmail.com>
2022-11-06 22:21:07 +0300

    ДЗ 7. Задание 1. Вносит правки от наставника

--- js/card.js
+++ js/card.js
@@ -1,70 +1,115 @@
-import { createAdverts } from './data.js';
-import { getCase } from './util.js';
-
-const map = document.querySelector('#map-canvas');
+import {getCase} from './util.js';
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 
 const typesTranslation = {
-  'flat': 'Квартира',
-  'bungalow': 'Бунгало',
-  'house': 'Дом',
-  'palace': 'Дворец',
-  'hotel': 'Отель',
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
+// Возвращает элемент с пользовательскими данными, если данных нет - удаляет элемент
+const pasteCardText = (parent, cssClass, data, additionalText = '') => {
+  const element = parent.querySelector(cssClass);
+  if (!data) {
+    element.remove();
+    return;
+  }
+  element.textContent = `${data}${additionalText}`;
+};
+
+// Возвращает ссылку на аватар пользователя, если данных нет - удаляет аватар
+const pasteAvatar = (parent, cssClass, data) => {
+  const element = parent.querySelector(cssClass);
+  if (!data) {
+    element.remove();
+    return;
+  }
+  element.src = data;
+};
+
+// Возвращает фото, загруженные пользователем, если данных нет - удаляет блок для фото
+const pastePhotos = (parent, cssClass, data) => {
+  const element = parent.querySelector(cssClass);
+  if (!data.length) {
+    element.remove();
+    return;
+  }
+  const photoItem = element.querySelector('.popup__photo');
+  element.innerHTML = '';
+  data.forEach((item) => {
+    const photoElement = photoItem.cloneNode(true);
+    photoElement.src = item;
+    element.append(photoElement);
+  });
 };
 
 
+// Возвращает список особенностей, отмеченных пользователем, если данных нет - удаляет блок особенностей
+const pasteFeatures = (parent, cssClass, data) => {
+  const element = parent.querySelector(cssClass);
+  if (!data.length) {
+    element.remove();
+    return;
+  }
+  const featureItems = element.querySelectorAll('.popup__feature');
+  const modifiers = data.map((feature) => `popup__feature--${feature}`);
+  featureItems.forEach((featureItem) => {
+    const modifier = featureItem.classList[1];
+    if (!modifiers.includes(modifier)) {
+      featureItem.remove();
+    }
+  });
+};
+
+// Возвращает количество комнат/гостей, выбранное пользователем, если данных нет - удаляет блок вместимости
+const createCapacityText = (dataRooms, dataGuests) => {
+  if (!dataRooms && !dataGuests) {
+    return '';
+  }
+  if (dataRooms && !dataGuests) {
+    return `${dataRooms} ${getCase(dataRooms, ['комната', 'комнаты', 'комнат',])} `;
+  }
+  if (!dataRooms && dataGuests) {
+    return `Для ${dataGuests} ${getCase(dataGuests, ['гостя', 'гостей', 'гостей'])}`;
+  }
+  if (dataRooms && dataGuests) {
+    return `${dataRooms} ${getCase(dataRooms, ['комната', 'комнаты', 'комнат',])}
+    для ${dataGuests} ${getCase(dataGuests, ['гостя', 'гостей', 'гостей'])}`;
+  }
+};
+
+// Возвращает время заезда/выезда, выбранное пользователем, если данных нет - удаляет блок времени
+const createTimeText = (dataTimeIn, dataTimeOut) => {
+  if (!dataTimeIn && !dataTimeOut) {
+    return '';
+  }
+  if (dataTimeIn && !dataTimeOut) {
+    return `Заезд после ${dataTimeIn}`;
+  }
+  if (!dataTimeIn && dataTimeOut) {
+    return `Выезд до ${dataTimeOut}`;
+  }
+  if (dataTimeIn && dataTimeOut) {
+    return `Заезд после ${dataTimeIn}, выезд до ${dataTimeOut}`;
+  }
+};
+
 // Создает карточку с объявлением
-const createCard = ({ author, offer }) => {
+const createCard = ({author, offer}) => {
   const cardElement = cardTemplate.cloneNode(true);
-
-  // Подставляет пользовательские данные в карточку
-  cardElement.querySelector('.popup__avatar').src = author.avatar;
-  cardElement.querySelector('.popup__title').textContent = offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = offer.address;
-  cardElement.querySelector('.popup__text--price').textContent = `${offer.price} '₽/ночь'`;
-  cardElement.querySelector('.popup__type').textContent = typesTranslation[offer.type];
-  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${getCase(offer.rooms, ['комната', 'комнаты', 'комнат'])} для ${offer.guests} ${getCase(offer.guests, ['гостя', 'гостей', 'гостей'])}`;
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-
-  // Проверяет, есть ли у объявления особенности (features), если да - добавляет все перечисленные пользователем особенности в карточку, если нет - блок с особенностями скрывается
-  const featuresList = cardElement.querySelector('.popup__features');
-  const featureItems = featuresList.querySelectorAll('.popup__feature');
-  if (offer.features && offer.features.length) {
-    const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
-    featureItems.forEach((featureItem) => {
-      const modifier = featureItem.classList[1];
-      if (!modifiers.includes(modifier)) {
-        featureItem.remove();
-      }
-    });
-  } else {
-    featuresList.remove();
-  }
-
-  // Проверяет, есть ли у объявления описание (description), если да - добавляет пользовательский текст в карточку, если нет - блок с описание скрывается
-  if (offer.description && offer.description.length) {
-    cardElement.querySelector('.popup__description').textContent = offer.description;
-  } else {
-    cardElement.querySelector('.popup__description').remove();
-  }
-
-  // Проверяет, есть ли у объявления фотографии (photos), если да - добавляет все имеющиеся фото в карточку, если нет - блок с фотографиями скрывается
-  const photosList = cardElement.querySelector('.popup__photos');
-  const photoItem = photosList.querySelector('.popup__photo');
-  if (offer.photos && offer.photos.length) {
-    photosList.innerHTML = '';
-    offer.photos.forEach((photo) => {
-      const photoElement = photoItem.cloneNode(true);
-      photoElement.src = photo;
-      photoElement.alt = offer.title;
-      photosList.append(photoElement);
-    });
-  } else {
-    photosList.remove();
-  }
-
+  pasteAvatar(cardElement, '.popup__avatar', author.avatar);
+  pasteCardText(cardElement, '.popup__title', offer.title);
+  pasteCardText(cardElement, '.popup__text--address', offer.address);
+  pasteCardText(cardElement, '.popup__text--price', offer.price, ' ₽/ночь');
+  pasteCardText(cardElement, '.popup__type', typesTranslation[offer.type]);
+  pasteCardText(cardElement, '.popup__text--capacity', createCapacityText(offer.rooms, offer.guests));
+  pasteCardText(cardElement, '.popup__text--time', createTimeText(offer.checkin, offer.checkout));
+  pasteFeatures(cardElement, '.popup__features', offer.features);
+  pasteCardText(cardElement, '.popup__description', offer.description);
+  pastePhotos(cardElement, '.popup__photos', offer.photos);
   return cardElement;
 };
 
-// Отрисовывает карточку объявления в блоке для вставки карты
-map.append(createCard(createAdverts()[0]));
+export {createCard};
--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-import './card.js';
+import { createAdverts } from './data.js';
+import {createCard} from './card.js';
+
+// Находит блок для вставки карточки с объявлением
+const map = document.querySelector('#map-canvas');
+
+// Отрисовывает карточку объявления в блоке для вставки карты
+map.append(createCard(createAdverts()[0]));
 
